9d9e6c2bd331a19f1b8a2cfbe78e442a18846396
Manish Kumar <manugud03@gmail.com>
2021-02-14 19:07:05 +0530

    some more files

---
+++ streetnumandname.py
@@ -0,0 +1,21 @@
+#The format_address function separates out parts of the address string into new strings: house_number and street_name, and returns: "house number X on street named Y". The format of the input string is: numeric house number, followed by the street name which may contain numbers, but never by themselves, and could be several words long. For example, "123 Main Street", "1001 1st Ave", or "55 North Center Drive". Fill in the gaps to complete this function.
+def format_address(address_string):
+  # Declare variables
+    house_number = ""
+    street_name = ""
+  # Separate the address string into parts
+    parts = address_string.split(" ")
+    house_number = parts[0]
+    for part in parts[1:]:
+        street_name = street_name + " " + part                    
+    return "house number {} on street named {}".format(house_number,street_name)
+
+print(format_address("123 Main Street"))
+# Should print: "house number 123 on street named Main Street"
+
+print(format_address("1001 1st Ave"))
+# Should print: "house number 1001 on street named 1st Ave"
+
+print(format_address("55 North Center Drive"))
+# Should print "house number 55 on street named North Center Drive"
+
