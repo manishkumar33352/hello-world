be50e916c1c8942ffcd4a4cb0effc39501f7ca7c
Manish Kumar <manugud03@gmail.com>
2021-03-08 13:24:08 +0530

    more python files

--- cod.py
+++ cod.py
@@ -1,3 +1,20 @@
-colors = ["red", "white", "blue"]
-colors.insert(2, "yellow")
-print (colors)
+class Friends:
+  name = ""
+  typ = ""
+  def __init__(self,name,typ):
+    self.name = name
+    self.type = typ
+    pass
+  def __str__(self):
+    return "Friend with name {} is very {}".format(self.name,self.type)
+  def meet_friend(self):
+    return "Friend with name {} meet with a new friend".format(self.name)
+  
+friend = Friends("Mishal","Sweet")
+print(friend)
+
+friend2 = Friends("Manish","nice")
+print(friend2)
+
+
+
